0d8e40b896e9beec78ce1732f93a7ba25edc589e
A007connor <adrien.rolo29@gmail.com>
2024-04-02 23:50:02 +0200

    Update Gameplay.cs

--- Assets/Memory/Scripts/Gameplay.cs
+++ Assets/Memory/Scripts/Gameplay.cs
@@ -1,20 +1,28 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class Gameplay : MonoBehaviour
 {
+    private GameObject[] bloc;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        bloc = GameObject.FindGameObjectsWithTag("Bloc");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                SpriteRenderer spriteRenderer = hit.transform.Find("Sprite").GetComponent<SpriteRenderer>();
+                spriteRenderer.enabled = !spriteRenderer.enabled;
+            }
+        }
     }
 }
