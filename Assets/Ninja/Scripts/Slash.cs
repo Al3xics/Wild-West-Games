75d2be4a5bb25f8f6aaf046443ce66f8c9049bb1
Brieuc LE BLOCH <bleblochpgaming.tech>
2024-04-02 17:18:50 +0200

    build 1

--- Assets/Ninja/Scripts/Slash.cs
+++ Assets/Ninja/Scripts/Slash.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
-using static UnityEngine.GraphicsBuffer;
 
 public class Slash : MonoBehaviour
 {
