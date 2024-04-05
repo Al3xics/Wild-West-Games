5f7756812069618de2590573052b77fd057cd589
Alexis <alexis.martinho.78@gmail.com>
2024-04-05 15:27:32 +0200

    Menu & Settings & Games
    Premier essaie sur l'UI

---
+++ Assets/UI/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void PlayGameButton()
+    {
+        //SceneManager.LoadScene("......");
+    }
+
+    public void SettingsButton()
+    {
+
+    }
+
+    public void GamesButton()
+    {
+
+    }
+
+    public void MusicButton()
+    {
+
+    }
+
+    public void SoundEffectsButton()
+    {
+
+    }
+
+    public void LanguagesButton()
+    {
+
+    }
+
+    public void ProgressionButton()
+    {
+
+    }
+
+    public void AboutUsButton()
+    {
+
+    }
+
+    public void BackButton()
+    {
+
+    }
+}
