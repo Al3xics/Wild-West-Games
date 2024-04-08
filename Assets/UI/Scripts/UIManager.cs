7b36e3bcf7a2c1c3c101a4d1a9b6c283ea3f9745
Alexis <alexis.martinho.78@gmail.com>
2024-04-08 14:48:01 +0200

    Deplacement d'un bouton à l'autre

--- Assets/UI/Scripts/UIManager.cs
+++ Assets/UI/Scripts/UIManager.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject games;
+
+    // L'objet en haut de la pile est l'objet actuel sur lequel on se situe
+    private Stack<GameObject> objectStack = new();
+
+    void Start()
+    {
+        objectStack.Push(menu);
+    }
+
     public void PlayGameButton()
     {
         //SceneManager.LoadScene("......");
@@ -12,12 +24,16 @@ public class UIManager : MonoBehaviour
 
     public void SettingsButton()
     {
-
+        objectStack.Push(settings);
+        menu.SetActive(false);
+        settings.SetActive(true);
     }
 
     public void GamesButton()
     {
-
+        objectStack.Push(games);
+        menu.SetActive(false);
+        games.SetActive(true);
     }
 
     public void MusicButton()
@@ -47,6 +63,14 @@ public class UIManager : MonoBehaviour
 
     public void BackButton()
     {
-
+        if (objectStack.Count > 0)
+        {
+            objectStack.Pop().SetActive(false);
+            objectStack.Peek().SetActive(true);
+        }
+        else
+        {
+            Debug.LogWarning("Il n'y a pas d'objet précédent à activer.");
+        }
     }
 }
