06e4fd72c5fb57b55f800f61f94594da2a5babed
Bastien91220 <bcrosnier@gaming.tech>
2024-04-11 16:16:06 +0200

    jauge terminée

--- Assets/Jauge/Scripts/slider.cs
+++ Assets/Jauge/Scripts/slider.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class slider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] Slider mySlider;
+
+    private bool canMove = true;
+    private bool maxVal = false;
+    private bool minVal = true;
+
+     public float myVal = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        mySlider.value = myVal;
+
+        if(myVal >= 1)
+        {
+            myVal = 1;
+        }
+
+        if (myVal <= 0)
+        {
+            myVal = 0;
+        }
+
+        if (canMove)
+        {
+            canMove = false;
+            StartCoroutine(addValue(0.01f));
+        }
+
+    }
+
+    private IEnumerator addValue(float a) 
+    {
+
+            if (minVal)
+            {
+                myVal += 0.01f;
+
+                if (myVal >= 1)
+                {
+                    maxVal = true;
+                    minVal = false;
+                }
+            }
+
+            if (maxVal)
+            {
+                myVal -= 0.01f;
+
+                if (myVal <= 0)
+                {
+                    maxVal = false;
+                    minVal = true;
+                }
+            }
+
+
+        yield return new WaitForSeconds(a);
+        canMove = true;
     }
 }
