3750ceaa3cf6890f4c9499a69cf21a5edede1617
Gtznut3 <aarbiol@gaming.tech>
2024-04-23 11:24:28 +0200

    DELETE COMMENTAIRE

--- Assets/Enlever Pubs/Scripts/EnleverPubsLevelManager.cs
+++ Assets/Enlever Pubs/Scripts/EnleverPubsLevelManager.cs
@@ -21,7 +21,6 @@ public class EnleverPubsLevelManager : MonoBehaviour
     {
         pubsList.Clear();
         timer = timerDuration;
-        //UIRoot = GameObject.Find("Canvas");
 
         float currentDifficultyLevel = GameManager.Instance.Difficulty / 2;
         difficultyLevel = Mathf.RoundToInt(currentDifficultyLevel);
@@ -40,34 +39,6 @@ public class EnleverPubsLevelManager : MonoBehaviour
             UpdateTimerUI();
         }
     }
-/*    private void OnDrawGizmos()
-    {
-        // Obtenir la caméra principale
-        Camera mainCamera = Camera.main;
-
-        if (mainCamera == null)
-        {
-            Debug.LogWarning("No main camera found. Please ensure there is a camera tagged as 'MainCamera'.");
-            return;
-        }
-
-        // Obtenir les dimensions de la vue de la caméra
-        float cameraHeight = mainCamera.orthographicSize * 2f;
-        float cameraWidth = cameraHeight * mainCamera.aspect;
-
-        // Calculer les coins de la zone de spawn
-        Vector3 topLeft = mainCamera.rect.position + new Vector2(-cameraWidth / 2f, cameraHeight / 2f);
-        Vector3 topRight = mainCamera.rect.position + new Vector2(cameraWidth / 2f, cameraHeight / 2f);
-        Vector3 bottomLeft = mainCamera.rect.position + new Vector2(-cameraWidth / 2f, -cameraHeight / 2f);
-        Vector3 bottomRight = mainCamera.rect.position + new Vector2(cameraWidth / 2f, -cameraHeight / 2f);
-
-        // Dessiner les lignes représentant la zone de spawn
-        Gizmos.color = Color.green;
-        Gizmos.DrawLine(topLeft, topRight);
-        Gizmos.DrawLine(topRight, bottomRight);
-        Gizmos.DrawLine(bottomRight, bottomLeft);
-        Gizmos.DrawLine(bottomLeft, topLeft);
-    }*/
 
     private void CreatePubs()
     {
@@ -75,19 +46,12 @@ public class EnleverPubsLevelManager : MonoBehaviour
         {
             for (int i = 0; i < numberOfPubs; i++)
             {
-                //Vector3 pos = Vector3.zero;
                 GameObject pubs = Instantiate(pubsPrefab);
                 pubs.transform.SetParent(UIRoot.transform, false);
                 RectTransform uiRectTransform = pubs.GetComponent<RectTransform>();
-                /*                pubs.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
-                                Vector3 value = RandomSpawnPosition();
-                                pubs.transform.position = Camera.main.ViewportToWorldPoint(new Vector3(value.x, value.y, Camera.main.nearClipPlane));*/
                 Vector3 value = RandomSpawnPosition();
                 uiRectTransform.anchoredPosition = new Vector2(value.x, value.y);
                 uiRectTransform.pivot = new Vector2(0.5f, 0.5f);
-                //pubs.transform.TransformPoint(pubs.transform.localPosition);
-                //pubs.transform.localPosition = new Vector3(pubs.transform.position.x, pubs.transform.position.y , 0);
-                //pubs.transform.position = RandomSpawnPosition();
                 pubsList.Add(pubs);
             }
         }
@@ -95,25 +59,18 @@ public class EnleverPubsLevelManager : MonoBehaviour
 
     private Vector3 RandomSpawnPosition()
     {
-/*               // Obtenir les dimensions de la vue de la caméra en pixels
-                float cameraWidthInPixels = Camera.main.pixelWidth;
-                float cameraHeightInPixels = Camera.main.pixelHeight;*/
         float panelWidth = UIRoot.GetComponent<RectTransform>().rect.width;
         float panelHeight = UIRoot.GetComponent<RectTransform>().rect.height;
 
-                // Calculer les coins du rectangle en fonction de la résolution de la caméra
-                Vector2 topRight = new(panelWidth / 3.8f, panelHeight / 2.8f);
-                Vector2 bottomLeft = new(-topRight.x, -topRight.y);
+        // Calculer les coins du rectangle en fonction de la résolution de la caméra
+        Vector2 topRight = new(panelWidth / 3.8f, panelHeight / 2.8f);
+        Vector2 bottomLeft = new(-topRight.x, -topRight.y);
 
-                float randomX = Random.Range(bottomLeft.x, topRight.x);
-                float randomY = Random.Range(bottomLeft.y, topRight.y);
-
+        float randomX = Random.Range(bottomLeft.x, topRight.x);
+        float randomY = Random.Range(bottomLeft.y, topRight.y);
 
         Debug.Log("width : " + panelWidth);
         Debug.Log("height : " + panelHeight);
-
-/*        float randomX = Random.Range(0, panelWidth);
-        float randomY = Random.Range(0, panelHeight);*/
 
         Vector3 randomPosition = new(randomX, randomY, 0f);
 
