c091eea0a908043898c2552b7bdea9c01a77a339
A007connor <adrien.rolo29@gmail.com>
2024-04-23 16:41:04 +0200

    Difficulty memory

--- Assets/Memory/Scripts/MemoryLvlManager.cs
+++ Assets/Memory/Scripts/MemoryLvlManager.cs
@@ -1,41 +1,113 @@
+using System.Collections;
 using UnityEngine;
 
 public class MemoryLvlManager : MonoBehaviour
 {
+    public static MemoryLvlManager instance;
+
     [SerializeField] private int level = 1;
-    [SerializeField] private float difficultyMultiplier = 1.0f;
-    private int numberOfPairsFound = 0;
+    [SerializeField] private int time = 10;
+    [SerializeField] private int score = 0;
 
-    public int Level
-    {
-        get { return level; }
-        set { level = value; }
-    }
+    private int rows, columns;
 
-    public float DifficultyMultiplier
+    private void Awake()
     {
-        get { return difficultyMultiplier; }
-        set { difficultyMultiplier = value; }
-    }
-
-    public int NumberOfPairsFound
-    {
-        get { return numberOfPairsFound; }
-    }
-
-    public void PairFound()
-    {
-        numberOfPairsFound++;
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
     void Start()
     {
-        int numberOfBlocks = GameplayTest.instance.blocs.GetLength(0) * GameplayTest.instance.blocs.GetLength(1);
-
-        if (numberOfBlocks >= 4 && numberOfBlocks <= 16)
+        if (GameManager.Instance != null) // Vérifiez si GameManager.Instance est null
         {
-            difficultyMultiplier = (numberOfBlocks - 4) / 2 + 1;
+            if (GameManager.Instance.Difficulty > 0 && GameManager.Instance.Difficulty <= 20)
+                level = 1;
+            else if (GameManager.Instance.Difficulty > 20 && GameManager.Instance.Difficulty <= 40)
+                level = 2;
+            else if (GameManager.Instance.Difficulty > 40 && GameManager.Instance.Difficulty <= 60)
+                level = 3;
+            else if (GameManager.Instance.Difficulty > 60 && GameManager.Instance.Difficulty <= 80)
+                level = 4;
+            else if (GameManager.Instance.Difficulty > 80 && GameManager.Instance.Difficulty <= 100)
+                level = 5;
+            else
+                level = 1; // Niveau par défaut
+        }
+        SetDifficulty(level);
+    }
+
+    public void SetDifficulty(int dif)
+    {
+        switch (dif)
+        {
+            case 1:
+                rows = 2;
+                columns = 2;
+                break;
+            case 2:
+                rows = 3;
+                columns = 2;
+                break;
+            case 3:
+                rows = 4;
+                columns = 2;
+                break;
+            case 4:
+                rows = 4;
+                columns = 3;
+                break;
+            case 5:
+                rows = 4;
+                columns = 4;
+                break;
+            default:
+                rows = 2;
+                columns = 2;
+                break;
         }
     }
 
+    public int Rows
+    {
+       get { return rows; }
+    }
+    public int Columns 
+    {
+        get { return columns; } 
+    }
+
+    public void EndGame(bool fin)
+    {
+        if (fin)
+        {
+            GameManager.Instance.WinMiniGame();
+            
+        }
+        else
+        {
+            GameManager.Instance.EndMiniGame();
+        }
+    }
+
+    public void SetScore(int value)
+    {
+        score = value;
+    }
+
+    public int GetScore()
+    {
+       return score;
+    }
+
+    public int GetLevel()
+    {
+        return level;
+    }
 }
