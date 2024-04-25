68e3037c9a413bfd080d331948ad60d7267f00d1
BrieucLEBLOCH <brieuc.leblochpro@gmail.com>
2024-04-25 14:47:17 +0200

    la difficulté marchait pas mdr

--- Assets/Ninja/Scripts/Spawner.cs
+++ Assets/Ninja/Scripts/Spawner.cs
@@ -9,14 +9,39 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject Bomb;
     private bool cd = true;
     private int timer = 0;
-    private float lvl = 1;
+    private float lvl;
     // Start is called before the first frame update
     void Start()
     {
-        foreach (GameObject go in GameObject.FindGameObjectsWithTag("LvlManager"))
+        if (GameManager.Instance.Difficulty > 0 && GameManager.Instance.Difficulty <= 20)
         {
-            lvl = go.GetComponent<NinjaLvlManager>().GetLvl();
+            lvl = 1;
         }
+        else if (GameManager.Instance.Difficulty > 20 && GameManager.Instance.Difficulty <= 40)
+        {
+            lvl = 2;
+        }
+        else if (GameManager.Instance.Difficulty > 40 && GameManager.Instance.Difficulty <= 60)
+        {
+            lvl = 3;
+        }
+        else if (GameManager.Instance.Difficulty > 60 && GameManager.Instance.Difficulty <= 80)
+        {
+            lvl = 4;
+        }
+        else if (GameManager.Instance.Difficulty > 80 && GameManager.Instance.Difficulty <= 100)
+        {
+            lvl = 5;
+        }
+        else
+        {
+            lvl = 1;
+        }
+        //foreach (GameObject go in GameObject.FindGameObjectsWithTag("LvlManager"))
+        //{
+        //    //lvl = go.GetComponent<NinjaLvlManager>().GetLvl();
+        //}
+        Debug.Log("Difficulty : " + lvl);
     }
 
     // Update is called once per frame
@@ -32,23 +57,21 @@ public class Spawner : MonoBehaviour
     private IEnumerator Spawn(float waitTime)
     {
         yield return new WaitForSeconds(waitTime);
-        for (int i = 0; i < 2+lvl; i++)
+        for (int i = 0; i < 1+lvl; i++)
         {
             Instantiate(Fruit, gameObject.transform.position, gameObject.transform.rotation);
         }
         if (lvl > 1)
         {
-            for (int i = 0; i < Random.Range(1, 3); i++)
+            for (int i = 0; i < Random.Range(1, 2+lvl); i++)
             {
-                if (Random.Range(1, 10) < 6)
-                {
-                    Instantiate(Fruit, gameObject.transform.position, gameObject.transform.rotation);
-                }
-                else
-                {
-                    Instantiate(Bomb, gameObject.transform.position, gameObject.transform.rotation);
-                }
+                Instantiate(Bomb, gameObject.transform.position, gameObject.transform.rotation);
             }
         }
+    }
+
+    public void SetLvl(float f)
+    {
+        lvl = f;
     }
 }
