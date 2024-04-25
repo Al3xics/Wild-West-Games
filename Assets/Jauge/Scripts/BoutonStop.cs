394644eee6bf694ff944fb2d643a9ca7a1293692
Bastien CROSNIER <bcrosnier@gaming.tech>
2024-04-25 17:28:07 +0200

    correction jauge et compteur

--- Assets/Jauge/Scripts/BoutonStop.cs
+++ Assets/Jauge/Scripts/BoutonStop.cs
@@ -14,12 +14,6 @@ public class BoutonStop : MonoBehaviour
     [SerializeField] GameObject handle;
     [SerializeField] Sprite newSprite;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
